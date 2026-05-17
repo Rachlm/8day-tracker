af1b9344012c712b2dddc39d74fa1d5f584c590a
Rachlm <pkmrxd@gmail.com>
2026-05-17 17:08:34 +0200

    Add files via upload

--- sw.backup.js
+++ sw.backup.js
@@ -1,4 +1,4 @@
-const CACHE = '8day-tracker-v64';
+const CACHE = '8day-tracker-v65';
 const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js'];
 
